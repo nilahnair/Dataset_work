ca95f62868e27afbf36b92e8703c0c8539a5bdbc
Nilah Ravi Nair <nilahnair.27@gmail.com>
2023-11-24 15:20:44 +0100

    Update preprocessing_sisfall.py

--- SisFall/preprocessing_sisfall.py
+++ SisFall/preprocessing_sisfall.py
@@ -186,6 +186,7 @@ def generate_data(ids, activities, sliding_window_length, sliding_window_step, d
                    print(len(segments))
                    segments=np.array([np.array(i) for i in segments])
                    print(segments.shape)
+                   print(segments)
                    all_segments=np.concatenate((all_segments, segments), axis=0)
                    print('len of all segments')
                    print(len(all_segments))
