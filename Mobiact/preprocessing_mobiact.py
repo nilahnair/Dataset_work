edd3385dba20d50b91ed71fb6752391c552f94ca
Nilah Ravi Nair <nilahnair.27@gmail.com>
2023-11-29 17:03:46 +0100

    Update preprocessing_mobiact.py

--- Mobiact/preprocessing_mobiact.py
+++ Mobiact/preprocessing_mobiact.py
@@ -12,7 +12,7 @@ import os
 import sys
 import matplotlib.pyplot as plt
 import datetime
-from sliding_window import sliding_window
+#from sliding_window import sliding_window
 import pickle
 import time
 import pandas as pd
