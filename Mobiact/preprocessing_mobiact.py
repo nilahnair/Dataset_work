8466e79dfc6adb0782a4ffdbe04a130d601108a7
Nilah Ravi Nair <nilahnair.27@gmail.com>
2023-12-05 12:39:14 +0100

    Update preprocessing_mobiact.py

--- Mobiact/preprocessing_mobiact.py
+++ Mobiact/preprocessing_mobiact.py
@@ -92,7 +92,7 @@ def reader_data(path):
             print(time)
             IMU.extend(list(map(float, row[2:11])))
             print(IMU)
-            label.extend(row[11])
+            label.extend(map(string, row[11]))
             print(label)
         
 
